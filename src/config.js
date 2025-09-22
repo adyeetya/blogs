aeb0b84764840f8db33e54cd602e7faea9299269
adyeetya <2612adityasingh2000@gmail.com>
2025-09-22 23:35:02 +0530

    ip fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.ts
-// export const SERVER_URL =  "https://backend.foundersmiddleeast.com";
+export const SERVER_URL =  "https://backend.foundersmiddleeast.com";
 
-export const SERVER_URL = "http://localhost:5009";
+// export const SERVER_URL = "http://localhost:5009";
