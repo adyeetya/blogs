c59715b9d9b348737d631ce83886d924550fa04f
adyeetya <2612adityasingh2000@gmail.com>
2025-09-27 01:30:32 +0530

    ip

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.ts
-// export const SERVER_URL =  "https://backend.foundersmiddleeast.com";
+export const SERVER_URL =  "https://backend.foundersmiddleeast.com";
 
-export const SERVER_URL = "http://localhost:5009";
+// export const SERVER_URL = "http://localhost:5009";
 
 // TODO : need to add types of everything have a seperate file for type of thiings like blogs and remvoe type any from places whereever possible
